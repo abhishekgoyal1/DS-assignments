fae6c290f601486a79cce0597894eeb8250b5b4e
abhishekgoyal1 <abhi.goyal2@gmail.com>
2018-01-21 20:11:07 +0530

    Add files via upload

---
+++ stack-deque/cStack.hpp
@@ -0,0 +1,111 @@
+#ifndef CSTACK_H
+#define CSTACK_H
+
+#include <iostream>
+
+using namespace std;
+
+template <typename E>
+class cStack {		// an interface for a stack
+
+private:
+    int *arr;
+    int top_red;
+    int top_blue;
+    int capacity;
+public:
+    cStack(int cap = 100);		// constructor from capacity
+    int redsize() const;		// number of items in stack
+    bool redempty() const;		// is the stack empty?
+    const E& redtop() const;	      		// the top element
+    void redpush(const E& e); 		// push x onto the stack
+    void redpop();		      	// remove the top element
+    int bluesize() const;               // number of items in stack
+    bool blueempty() const;             // is the stack empty?
+    const E& bluetop() const;           // the top element
+    void bluepush(const E& e);          // push x onto the stack
+    void bluepop();                     // remove the top element
+ };
+
+template <typename E>
+cStack<E>::cStack(int cap){
+    arr= new E[cap];
+    capacity=cap;
+    top_red=-1;
+    top_blue=capacity;};
+
+template <typename E>
+int cStack<E>::redsize() const{
+    return top_red +1;
+};
+
+template <typename E>
+bool cStack<E>::redempty() const{
+    if (top_red==-1)
+        return true;
+    else
+        return false;
+};
+
+template <typename E>
+const E& cStack<E>::redtop() const{
+    if (redempty()){
+        cout << "No element found" << endl;
+        //return;
+        }
+    else
+        return arr[top_red];
+};
+
+template <typename E>
+void cStack<E>::redpush(const E& e){
+    top_red++;
+    arr[top_red]= e;
+};
+
+template <typename E>
+void cStack<E>::redpop(){
+    if (redempty())
+        cout << "The red stack is empty" << endl;
+    else{
+        top_red--;}
+};
+
+template <typename E>
+int cStack<E>::bluesize() const{
+    return 100-top_blue;
+};
+
+template <typename E>
+bool cStack<E>::blueempty() const{
+    if (top_blue==100)
+        return true;
+    else
+        return false;
+};
+
+template <typename E>
+const E& cStack<E>::bluetop() const{
+    if (blueempty()){
+        cout << "No element found" << endl;}
+    else
+        return arr[top_blue];
+};
+
+template <typename E>
+void cStack<E>::bluepush(const E& e){
+    top_blue--;
+    arr[top_blue]= e;
+};
+
+template <typename E>
+void cStack<E>::bluepop(){
+    if (blueempty())
+        cout << "The blue stack is empty" << endl;
+    else{
+        top_blue++;}
+};
+
+
+
+#endif
---
+++ stack-deque/deque.hpp
@@ -0,0 +1,156 @@
+#ifndef DEQUE_H
+#define DEQUE_H
+
+#include <iostream>
+#include "cStack.hpp"
+
+template <typename E>
+class Deque {
+
+private:
+    cStack<E> A,B;
+public:
+    Deque();				// constructor
+    int size() const;				// number of items in the deque
+    bool empty() const;				// is the deque empty?
+    const E& front(); // the first element
+    const E& back();	// the last element
+    void insertFront(const E& e);		// insert new first element
+    void insertBack(const E& e);		// insert new last element
+    void removeFront();	// remove first element
+    void removeBack();	// remove last element
+  };
+
+template <typename E>
+Deque<E>::Deque(){
+
+};
+
+
+template <typename E>
+int Deque<E>::size() const{
+return A.redsize()+A.bluesize()+B.redsize();};
+
+
+template <typename E>
+bool Deque<E>::empty() const{
+if (A.redempty() && A.blueempty() && B.redempty())
+    return true;
+else
+    return false;
+};
+
+template <typename E>
+const E& Deque<E>::front(){
+    if (!A.redempty()){
+        return A.redtop();
+    }
+    else if (!A.blueempty()){
+        E temp;
+        while (!A.blueempty()){
+            temp= A.bluetop();
+            A.redpush(temp);
+            A.bluepop();
+        }
+        return A.redtop();
+    }
+    else if (!B.redempty()){
+        while (!B.redempty()){
+            A.bluepush(B.redtop());
+            B.redpop();
+        }
+        return A.bluetop();
+    }
+    else{
+        cout << "The Deque is empty. Error" << endl;
+    }
+
+};
+
+template <typename E>
+const E& Deque<E>::back(){
+    if (!B.redempty()){
+        return B.redtop();
+    }
+    else if (!A.blueempty()){
+        //E temp;
+        while (!A.blueempty()){
+            B.redpush(A.bluetop());
+            A.bluepop();
+        }
+        return B.redtop();
+    }
+    else if (!A.redempty()){
+        while (!A.redempty()){
+            A.bluepush(A.redtop());
+            A.redpop();
+        }
+        return A.bluetop();
+    }
+    else{
+        cout << "The Deque is empty. Error" << endl;
+    }
+};
+
+template <typename E>
+void Deque<E>::insertFront(const E& e){
+A.redpush(e);};
+
+template <typename E>
+void Deque<E>::insertBack(const E& e){
+B.redpush(e);};
+
+template <typename E>
+void Deque<E>::removeFront(){
+    if (!A.redempty()){
+        A.redpop();
+    }
+    else if (!A.blueempty()){
+        //E temp;
+        while (!A.blueempty()){
+            A.redpush(A.bluetop());
+            A.bluepop();
+        }
+        A.redpop();
+    }
+    else if (!B.redempty()){
+        while (!B.redempty()){
+            A.bluepush(B.redtop());
+            B.redpop();
+        }
+        A.bluepop();
+    }
+    else{
+        cout << "The Deque is empty. Error" << endl;
+    }
+
+};
+
+template <typename E>
+void Deque<E>::removeBack(){
+    if (!B.redempty()){
+        B.redpop();
+    }
+    else if (!A.blueempty()){
+        //E temp;
+        while (!A.blueempty()){
+            B.redpush(A.bluetop());
+            A.bluepop();
+        }
+        B.redpop();
+    }
+    else if (!A.redempty()){
+        while (!A.redempty()){
+            A.bluepush(A.redtop());
+            A.redpop();
+        }
+        A.bluepop();
+    }
+    else{
+        cout << "The Deque is empty. Error" << endl;
+    }
+};
+
+
+
+#endif
